e8b7def012906cd686bc771b8842fcd9772b93e1
Simpz78 <sim.pietrelli@gmail.com>
2021-06-17 11:48:50 +0200

    Aggiunto riferimento a Rabbit e controller per il post del messaggio

---
+++ WPPublisher/Actions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WPPublisher.Controller;
+using WPPublisher.Model;
+
+namespace WPPublisher
+{
+    public class Actions
+    {
+        private WPPostController _WPPostController = new WPPostController();
+
+        public int PostMessage(WPPost post)
+        {
+            // Chiamata al controller per l'accodamento al rabbit
+             
+            // Chiamata al controller per la pubblicazione effettiva del messaggio
+
+            return 0;
+        }
+    }
+}
--- WPPublisher/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WPPublisher
-{
-    public class Class1
-    {
-    }
-}
