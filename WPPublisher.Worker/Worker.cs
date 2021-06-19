5d06079a721ff58277b104bd8beafc2f61bd7959
Simpz78 <sim.pietrelli@gmail.com>
2021-06-19 12:27:54 +0200

    Implemetata funzione per fare publish di un post

--- WPPublisher.Worker/Worker.cs
+++ WPPublisher.Worker/Worker.cs
@@ -1,8 +1,12 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Exceptions;
+using RestSharp;
+using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,50 +43,51 @@ namespace WPPublisher.Worker
         private void ConsumeMessage()
         {
             var factory = new ConnectionFactory() { HostName = _queueServer };
-            
-            using var connection = factory.CreateConnection();
-            using var channel = connection.CreateModel();
-            channel.ExchangeDeclarePassive(_queueExchange);
 
-            channel.QueueDeclarePassive(_queueName);
-            Console.WriteLine($"Queue [{_queueName}] is waiting for messages.");
-
-            var messageCount = channel.MessageCount(_queueName);
-            if (messageCount > 0)
+            using (var connection = factory.CreateConnection())
             {
-                Console.WriteLine($"\tDetected {messageCount} message(s).");
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclarePassive(_queueExchange);
+                    channel.QueueDeclarePassive(_queueName);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (bc, ea) =>
+                    {
+                        var t = DateTimeOffset.FromUnixTimeMilliseconds(ea.BasicProperties.Timestamp.UnixTime);
+                                               
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        PostMessage(message);
+                        try
+                        {
+                            Thread.Sleep((new Random().Next(1, 3)) * 1000);
+                            var model = ((IBasicConsumer)bc).Model;
+                            model.BasicAck(ea.DeliveryTag, false);
+                        }
+                        catch (AlreadyClosedException)
+                        {
+                            //Console.WriteLine("RabbitMQ is closed!");
+                        }
+                    };
+                    channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+                }
+
+                connection.Close();
             }
+        }
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (bc, ea) =>
-            {
-                var t = DateTimeOffset.FromUnixTimeMilliseconds(ea.BasicProperties.Timestamp.UnixTime);
-                Console.WriteLine($"{t.LocalDateTime:O} ID=[{ea.BasicProperties.MessageId}]");
-                var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-                Console.WriteLine($"\tProcessing message: '{message}'.");
+        private void PostMessage(string message)
+        {
+            JObject jo = (JObject)JsonConvert.DeserializeObject(message);
+            string title = jo["Title"].ToString();
+            string content = jo["Content"].ToString();
 
-                if (ea.BasicProperties.UserId != "ops0")
-                {
-                    Console.WriteLine($"\tIgnored a message sent by [{ea.BasicProperties.UserId}].");
-                    return;
-                }
+            var client = new RestClient("http://localhost:8080");
+            client.Authenticator = new HttpBasicAuthenticator("test", "test");
 
-                try
-                {
-                    Thread.Sleep((new Random().Next(1, 3)) * 1000);
-                    var model = ((IBasicConsumer)bc).Model;
-                    model.BasicAck(ea.DeliveryTag, false);
-                }
-                catch (AlreadyClosedException)
-                {
-                    Console.WriteLine("RabbitMQ is closed!");
-                }
-            };
-            channel.BasicConsume(queue: _queueName, autoAck: false, consumer: consumer);
-
-            Console.WriteLine("Press [enter] to exit.");
-            Console.ReadLine();
-            connection.Close();
+            var request = new RestRequest("?rest_route=/wp/v2/posts", Method.POST);
+            request.AddParameter("title", title);
+            request.AddParameter("content", content);
+            IRestResponse response = client.Execute(request);
         }
     }
 }
