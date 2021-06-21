3acbecfef2bb68c8bf3c9359d1cc2b3ce6398dd3
Simpz78 <sim.pietrelli@gmail.com>
2021-06-21 14:42:42 +0200

    Aggiunto progetto API

---
+++ WPPublisher.API/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WPPublisher.Model;
+
+namespace WPPublisher.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WPPost GetPost()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public IEnumerable<WPPost> GetPosts()
+        {
+            return null;
+        }
+    }
+}
