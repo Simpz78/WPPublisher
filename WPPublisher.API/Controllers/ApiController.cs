de20f037a0d93b937ad37b512719271ce32f8852
Simpz78 <sim.pietrelli@gmail.com>
2021-06-22 14:20:52 +0200

    Inizio funzioni per retrieve post

--- WPPublisher.API/Controllers/ApiController.cs
+++ WPPublisher.API/Controllers/ApiController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestSharp;
+using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,14 +22,29 @@ namespace WPPublisher.API.Controllers
         }
 
         [HttpGet]
-        public WPPost GetPost()
+        public WPPost GetPost(string idPost)
         {
+            // author, status
+            var client = new RestClient("http://localhost:8080");
+            //client.Authenticator = new HttpBasicAuthenticator("test", "test");
+
+            var request = new RestRequest("?rest_route=/wp/v2/posts" + idPost, Method.GET);
+            IRestResponse response = client.Execute(request);
+
             return null;
         }
 
         [HttpGet]
-        public IEnumerable<WPPost> GetPosts()
+        public IEnumerable<WPPost> GetPosts(int author, string status)
         {
+            var client = new RestClient("http://localhost:8080");
+            //client.Authenticator = new HttpBasicAuthenticator("test", "test");
+
+            var request = new RestRequest("?rest_route=/wp/v2/posts", Method.GET);
+            request.AddParameter("author", author);
+            request.AddParameter("status", status);
+            IRestResponse response = client.Execute(request);
+
             return null;
         }
     }
